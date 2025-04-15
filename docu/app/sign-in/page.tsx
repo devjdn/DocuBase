0cb5907a101a3782b157c5d874326b84fbf4a385
Jayden Priestley <jaydenUX@outlook.com>
2025-04-15 15:28:53 +0100

    Update middleware and sitemap for new vault structure; redirect signed-in users to vault page

--- docu/app/sign-in/page.tsx
+++ docu/app/sign-in/page.tsx
@@ -1,6 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+export default async function SignInPage() {
+	const user = await currentUser();
 
-export default function SignInPage() {
+	if (user) {
+		redirect("/vault");
+	}
+
 	return (
 		<div className="flex h-screen w-screen items-center justify-center">
 			<SignIn />
--- docu/app/sitemap.ts
+++ docu/app/sitemap.ts
@@ -1,18 +1,18 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://docubasedev.com',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://docubasedev.com/links',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-  ]
+	return [
+		{
+			url: "https://docubasedev.com",
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 1,
+		},
+		{
+			url: "https://docubasedev.com/vault",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 1,
+		},
+	];
 }
